a530cc5e9f71cbdb83b3532024f1fcff467c35b2
Euanher <70320894+Euanher@users.noreply.github.com>
2022-01-04 16:43:33 +0200

    update

---
+++ mycv_service/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mycv_service
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        private static void Main()
+        {
+            ServiceBase[] ServicesToRun = new ServiceBase[]
+{
+                new Service1()
+};
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
---
+++ node-output/User_Replace.sql
@@ -0,0 +1,24 @@
+CREATE  VIEW User_Replace AS 
+( Select true AS 'TABLE values' union all
+ (SELECT values ( 'select' or replace (second('select' "all union") and var_pop(locate ('table' 'select' in ('idLogin_Logcol'(log)) (select) [values]) with database(local)) add to abs within(table) (values)) all values in space( VIEW(table value)) sec_to_time(asbbinary(ord(return[])))
+ to_base64(abs(atan2( interiorringn( database data( values))
+ values(validate_password_strength((asbbinary(Locate('load_file()')))) 
+ st_contains(Register_Log[]abs id ))
+ abs(pointfromtext(replace or change [values]))
+ (with values[abs(id)])
+ insert( into 'schema Table value[id]' with session_user(tan( sum(*)into table values from Register_Log)[mydb]) 'table values(locate values from[idLogin_Logcol]'))
+acos(change NULL))
+ connection_id(binary id[] primary key))
+ asbinary( gtid_subset(wait_until_sql_thread_after_gtids(id))) 
+WITHIN( REPLACE OR 'CHANGE'(SELECT idUser_Logcol[INT])) load into Local database table )) 
+TABLE(SELECT generated(table values)'CHANGE OR REPLACE(values)') 
+AS
+ within( asbinary(TABLE(CHECK  database table 'idRegister_Log')))
+ with LOCAL check option 
+ (ALTER UNION  AS GENERATED  DATABASE TABLE(VALUES) 
+ FOR 
+ idUser_Logcol('CHANGE'REPLACE) within('Table'(value 'Register_Log')
+ as    
+SELECT User_name, User_email, User_password, User_QUESTION
+FROM idUser_Logcol
+WHERE User_Log  = 'User_name'
